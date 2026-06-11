2e4439b41399022442edcb5664878497bb02261e
PocketStudio12 <pocketstudio12@gmail.com>
2026-06-11 14:39:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ app.get("/", (req, res) => {
    START SERVER
 ========================= */
 
-const PORT = process.env.PORT || 20331;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(
